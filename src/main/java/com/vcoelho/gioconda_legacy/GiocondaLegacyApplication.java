61be776fc47d44c45682334dce85ad2d00f04fbe
vdcoelho <vicente.coelho@gmail.com>
2022-12-21 18:09:17 -0300

    Initial commit

---
+++ src/main/java/com/vcoelho/gioconda_legacy/GiocondaLegacyApplication.java
@@ -0,0 +1,13 @@
+package com.vcoelho.gioconda_legacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiocondaLegacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiocondaLegacyApplication.class, args);
+	}
+
+}
